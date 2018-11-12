d7c714040e9b372623caa1cdf7f69bbe19f67ed3
Giwan Persaud <giwan.persaud@gmail.com>
2018-11-12 16:53:17 +0100

    replaces images

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,41 +1,41 @@
 module.exports = {
-  siteMetadata: {
-    title: 'Mytoori blog',
-    author: 'Giwan Persaud',
-  },
-  plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/posts`,
-        name: 'markdown-pages',
-      },
+    siteMetadata: {
+        title: "Mytoori blog",
+        author: "Giwan Persaud",
     },
-    `gatsby-transformer-remark`,
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#fefefe',
-        theme_color: '#212121',
-        display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
-  ],
-}
+    plugins: [
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/src/posts`,
+                name: "markdown-pages",
+            },
+        },
+        `gatsby-transformer-remark`,
+        "gatsby-plugin-react-helmet",
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        "gatsby-transformer-sharp",
+        "gatsby-plugin-sharp",
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: "gatsby-starter-default",
+                short_name: "starter",
+                start_url: "/",
+                background_color: "#fefefe",
+                theme_color: "#212121",
+                display: "minimal-ui",
+                icon: "src/images/mytoori-news.svg", // This path is relative to the root of the site.
+            },
+        },
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.app/offline
+        // 'gatsby-plugin-offline',
+    ],
+};
