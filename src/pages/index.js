9a1b8549301cf27d6612c300f9e74e50ece8047e
Giwan Persaud <giwan.persaud@gmail.com>
2020-01-13 21:17:11 +0100

    rename to mainMenu

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
-import Menu from "../components/menu"
+import Menu from "../components/mainMenu"
 import PostItem from "../components/postItem"
 import "../style/index.css"
 
