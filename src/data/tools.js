7232f6fa76a97cfba3584d0d1b02d26e8650e106
Giwan Persaud <giwan.persaud@gmail.com>
2020-12-12 11:42:06 +0100

    updated tools section

---
+++ src/data/tools.js
@@ -0,0 +1,50 @@
+export const categories = Object.freeze({
+    NOTES: "Notes",
+    DESIGN: "Design",
+    EMAIL: "Email",
+    MARKETING: "Marketing",
+    PROJECT_MANAGEMENT: "Project Management",
+    WRITING: "Writing",
+})
+
+export const labels = Object.freeze({
+    design: "design",
+    inspiration: "inspiration",
+    productivity: "productivity",
+    education: "education",
+    marketing: "marketing",
+    leisure: "leisure",
+    project: "project",
+    management: "management",
+    writing: "writing",
+})
+
+const tools = [
+    {
+        title: "Figma",
+        url: "https://figma.com",
+        description:
+            "App great design tool. It's easy to use and especially get started. No installation required for example. Simply fireup your web browser and signup and get started.",
+        category: categories.DESIGN,
+        labels: [labels.design, labels.productivity],
+    },
+    {
+        title: "Trello",
+        url: "https://trello.com",
+        description: "A project management tools. Great for new projects.",
+        price: 0,
+        category: categories.PROJECT_MANAGEMENT,
+        labels: [labels.project, labels.management, labels.productivity],
+    },
+    {
+        title: "Google doc",
+        url: "https://trello.com",
+        description:
+            "Document writing. Create documents. If you've used Microsoft Word, this is a capable online alternative from Google.",
+        price: 0,
+        category: categories.WRITING,
+        labels: [labels.productivity, labels.writing],
+    },
+]
+
+export default tools
--- src/pages/tools.js
+++ src/pages/tools.js
@@ -1,18 +1,44 @@
 import React from "react"
-import "../style/about.css"
+import "../style/tools.css"
 import Layout from "../components/layout"
 import Header from "../components/header"
-const AboutPage = () => {
-    return (
-        <Layout>
-            <Header />
-            <div className="mb-tools__container">
-                <h1>Tools</h1>
-                <h2><a href="https://figma.com">Figma</a></h2>
-                <p>App prototyping and design tool.</p>
-            </div>
-        </Layout>
-    )
-}
+import tools from "../data/tools"
+// Behance.net, Dribble.com
 
-export default AboutPage
+const ToolsPage = () => (
+    <Layout>
+        <Header />
+        <article className="mb-tools__container">
+            <h1>Tools</h1>
+            <p>
+                Some of the tools I use on a regular basis. These are typically
+                the tools I would recommend to others as well.
+            </p>
+
+            <section className="mb-tools_list">
+                {tools.map((tool) => (
+                    <ToolItem key={tool.title + tool.description} tool={tool} />
+                ))}
+            </section>
+        </article>
+    </Layout>
+)
+
+export default ToolsPage
+
+/**
+ * The tools item. This is basically a page item for that tool.
+ *
+ */
+const ToolItem = ({ tool }) => (
+    <article className="mb-tools_item">
+        <h2>
+            <a href={tool.url}>{tool.title}</a>
+        </h2>
+        <p>{tool.description}</p>
+        <footer>
+            {Array.isArray(tool.labels) &&
+                tool.labels.map((label) => <label key={label}>{label}</label>)}
+        </footer>
+    </article>
+)
